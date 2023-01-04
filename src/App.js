a237d1edbf69114fb454491a69d9f371634c5899
Kelvin Anderson <kelvinbusiness10@gmail.com>
2023-01-04 14:58:25 +0700

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import { AiFillLeftCircle, AiFillRightCircle } from "react-icons/ai";
 
 function App() {
+  const [index, setIndex] = useState(0)
+  const data = [
+    {locationName: 'Rawamangun'},
+    {locationName: 'Jawa Barat'},
+    {locationName: 'Jawa Barat'},
+    {locationName: 'Jawa Barat'},
+    {locationName: 'Jawa Barat'},
+    {locationName: 'Jawa Barat'},
+    {locationName: 'Jawa Barat'},
+    {locationName: 'Jawa Barat'},
+    {locationName: 'Jawa Barat'},
+    {locationName: 'Jawa Barat'},
+    {locationName: 'Jawa Barat'},
+    {locationName: 'Jawa Barat'},
+    {locationName: 'Jawa Barat'},
+    {locationName: 'Jawa Barat'},
+    {locationName: 'Jawa Barat'},
+    {locationName: 'Rawamangun'},
+  ]
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='outerContainer'>
+        <AiFillLeftCircle size={'40px'} color='#B70621' onClick={() => {
+          document.getElementById('location-container').scrollLeft -= 500;
+        }}></AiFillLeftCircle>
+      <div id='location-container' className='container'>
+        {data.map((item, i) => {
+          return(
+            <div className={`box ${index == i && 'my-checkfield-selected'}`} onClick={() => {
+              setIndex(i)
+            }}>
+              <p className='contentTitle'>{item.locationName}</p>
+              <p className='content'>{item.locationName}</p>
+            </div>
+          )
+        })}
+      </div>
+      <AiFillRightCircle size={'40px'} color='#B70621' onClick={() => {
+          document.getElementById('location-container').scrollLeft += 500;
+        }}></AiFillRightCircle>
+      </div>
     </div>
   );
 }
